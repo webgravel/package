ca8f3175263a7653e89d653730fb5d77976110e0
Michał Zieliński <michal@zielinscy.org.pl>
2013-07-24 20:56:18 +0200

    updaterepo.sh: don't show rsync progress

--- updaterepo.sh
+++ updaterepo.sh
@@ -5,11 +5,10 @@ REPO_PATH=/home/gravelrepo/
 packages=$GHOME/*/*.gravelpkg
 tmp=`mktemp -d`
 for pkg in $packages; do
-    echo $pkg
     cp $pkg $tmp/
 done
 
-rsync -r --progress $tmp/ $REPO_SRV:$REPO_PATH
+rsync -r $tmp/ $REPO_SRV:$REPO_PATH
 ssh $REPO_SRV chmod 644 $REPO_PATH'/*.gravelpkg'
 cd ~
 rm -r $tmp
